669e5cbd614883a454d5391e0f322dd3f9bd9f84
damian <damianc2@illinois.edu>
2019-12-02 22:34:59 -0600

    start

--- app/src/main/java/com/damian/ievents/MainActivity.java
+++ app/src/main/java/com/damian/ievents/MainActivity.java
@@ -1,38 +1,29 @@
 package com.damian.ievents;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-
-import com.damian.ievents.ui.main.SectionsPagerAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * button made for creating group.
+     */
+    private Button login;
+    private EditText Email;
+    private EditText Password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        Email = (EditText)findViewById(R.id.);
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, loginGroup.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/damian/ievents/loginGroup.java
@@ -0,0 +1,5 @@
+package com.damian.ievents;
+
+public class loginGroup {
+
+}
